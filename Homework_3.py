d9f167ed90c08c1a25cb70582f42a7080af5cce1
pengguo <guopengstar@qq.com>
2019-04-10 23:25:24 +0800

    add knn

--- Homework_3.py
+++ Homework_3.py
@@ -15,6 +15,10 @@ import sklearn.decomposition
 import sklearn.neighbors
 import sklearn.metrics
 
+
+# https://www.cnblogs.com/pinard/p/6061661.html
+
+
 def problem1():
     teams = pd.read_csv("./data/Teams.csv")
     players = pd.read_csv("./data/Batting.csv")
@@ -146,6 +150,16 @@ def problem1():
 
     print 'problem1 done!!!'
 
+def computeTestScores(test_x, test_y, clf, cv):
+    kFolds = sklearn.model_selection.KFold(n_splits=3, shuffle=False)
+
+    scores = []
+    for _, test_index in kFolds.split(test_x):
+        test_data = test_x[test_index]
+        test_labels = test_y[test_index]
+        scores.append(sklearn.metrics.accuracy_score(test_labels, clf.predict(test_data)))
+    return scores
+
 def problem2():
     # load the iris data set
     iris = sklearn.datasets.load_iris()
@@ -179,23 +193,73 @@ def problem2():
 
     print clf.best_score_, clf.best_params_
 
-    scores = [x for x in clf.cv_results_['mean_test_score']]
-
-    # score_means = np.mean(scores, axis=1)
-
-    sns.boxplot(scores)
-    # plt.scatter(k, score_means, c='k', zorder=2)
-    plt.ylim(0.8, 1.1)
-    plt.title('Accuracy as a function of $k$')
-    plt.ylabel('Accuracy')
-    plt.xlabel('Choice of k')
-    plt.show()
+    scores = computeTestScores(test_x=X_test, test_y=Y_test, clf=clf, cv=5)
+    print np.mean(scores), np.std(scores)
 
     print 'problem2 done!!!'
 
+def problem3():
+    digits = sklearn.datasets.load_digits()
+
+    X = digits.data
+    Y = digits.target
+
+    print X.shape, Y.shape
+    ### Your code here ###
+
+    X_train, X_test, Y_train, Y_test = sklearn.model_selection.train_test_split(
+        X, Y, test_size=0.33, random_state=42)
+
+    X_train_means = np.mean(X_train, axis=0)
+    X_train_centered = X_train - X_train_means
+    X_test_centered = X_test - X_train_means
+
+    print X_train.shape
+    print X_test.shape
+    print Y_train.shape
+    print Y_test.shape
+
+    svd = sklearn.decomposition.TruncatedSVD(n_components=2)
+    X_2d = svd.fit_transform(X_train_centered)
+
+    # fig, (ax1, ax2) = plt.subplots(nrows=2, figsize=(5,8))
+    # ax1.scatter(X_2d[:, 0], X_2d[:, 1], c=Y_train, s=50, cmap=plt.cm.Paired)
+    # # plt.colorbar()
+    # plt.xlabel('PC1')
+    # plt.ylabel('PC2')
+    # plt.title('First two PCs using digits data')
+    # plt.show()
+
+    # ind = np.logical_or(Y_train == 4, Y_train == 1)
+    # ax2.scatter(X_2d[ind, 0], X_2d[ind, 1], c=Y_train[ind], s=50, cmap=plt.cm.Paired)
+    # # plt.show()
+
+    # fit knn
+    k = np.arange(20) + 1
+    parameters = {'n_neighbors': k}
+    knn = sklearn.neighbors.KNeighborsClassifier()
+    clf = sklearn.model_selection.GridSearchCV(knn, parameters, cv=10)
+    all_scores = []
+    all_k = []
+    all_d = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+    for d in all_d:
+        print d
+        svd = sklearn.decomposition.TruncatedSVD(n_components=d)
+        X_d = svd.fit_transform(X_train_centered)
+        X_d_test = svd.transform(X_test_centered)
+
+        clf.fit(X_d, Y_train)
+
+        all_scores.append(computeTestScores(test_x=X_d_test, test_y=Y_test, clf=clf, cv=10))
+        all_k.append(clf.best_params_['n_neighbors'])
+    print all_scores
+    print all_k
+
 def main():
     # problem1()
-    problem2()
+    # problem2()
+    problem3()
 
 
 if __name__ == '__main__':
